1e85a2fb6b2cc2258a61e707822a05e81b9c0574
Rohit Tatu <rohit.tatu896@gmail.com>
2020-05-15 12:29:57 +0530

    Bottom Tabs

--- src/navigator/index.js
+++ src/navigator/index.js
@@ -1,24 +1,33 @@
-import {
-  createMaterialTopTabNavigator,
-  createAppContainer,
-} from 'react-navigation';
-import TabBar from '../component/TabBar';
+import * as React from 'react';
+import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-const TabNavigator = createMaterialTopTabNavigator(
-  {
-    feed: {
-      screen: () => null,
-    },
-    profile: {
-      screen: () => null,
-    },
-    inbox: {
-      screen: () => null,
-    },
-  },
-  {
-    tabBarComponent: TabBar,
-  },
-);
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
 
-export default createAppContainer(TabNavigator);
+function SettingsScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
